d677d2ebe2b9dfa49a978c4a0621fbab6b4ea1f8
Archabek07 <mr.archabek@gmail.com>
2021-06-20 19:01:57 +0600

    Add 'Calc' files

--- Grid_position/js/main.js
+++ Grid_position/js/main.js
@@ -1,4 +1,48 @@
-setTimeout(wakeUpUser, 2000);
+/*setTimeout(wakeUpUser, 2000);
 function wakeUpUser() {
   alert("Are you going to stare at this boring page forever?");
-}
+}*/
+
+// let userName = "Bambucha";
+// let question = "How old he is?";
+// let userAge = " He is 15";
+// alert(userName);
+// alert(question);
+// alert(userAge);
+
+// let a = 1;
+// let b = 2;
+// alert("Count a:" + a);
+// alert("Count b:" + b);
+
+// let replace = a;
+// a = b;
+// b = replace;
+
+// alert("Count a:" + a);
+// alert("Count b:" + b);
+
+// let user;
+
+// let name = "Alexey";
+// let admin = "Ivan";
+// alert(admin);
+
+// let solarsystemPlanet = "Earth";
+// let currentUserName$visiting = "Hanry";
+
+// let scoop = "10";
+// while (scoop > 0) {
+//   setTimeout(scoop, 2000);
+//   document.write("Еще одну ложечку? <br>");
+//   scoop = scoop - 1;
+// }
+// document.write("Ой, а мороженное закончилось.");
+
+let name = "Ilya";
+
+alert(`hello ${1}`); // выведет Hello 1
+
+// alert(`hello ${"name"}`); // Hello name
+
+// alert(`hello ${name}`); // Hello Ilya
